647f85f2b79fba16c0172f1c02483ff54b9f99c1
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2024-02-06 08:48:02 +0530

    updated

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello, this is a code by Ramanpreet to test pipeline and this is the second version');
-  });
-  const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
 });
